fc660a2c3105f0298b102fdbdc80837aab519034
DevManCryBB <veronicagriggs3@gmail.com>
2023-06-14 14:51:19 -0700

    added logic to mutation and queries in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,42 @@
 import User from '../models/User'
 
+module.exports = resolvers;
+
 const resolvers = {
     Query: {
-        user: async () => {
-            return User.find({})
-        },
-        
-    }
-}
+      me: async () => {
+        return User.find({})
+    },
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const profile = await Profile.findOne({ email });
+      
+            if (!profile) {
+              throw new AuthenticationError('No profile with this email found!');
+            }
+      
+            const correctPw = await profile.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect password!');
+            }
+      
+            const token = signToken(profile);
+            return { token, profile };
+          },
 
-module.exports = resolvers;
+      addUser: async (parent,{ username, email, password }, context) => {
+
+      },
+      saveBook: async (parent, { bookData }, context) => {
+        
+      },
+      removeBook: async (parent, { bookId }, context) => {
+       
+      },
+    },
+  };
+  
+  module.exports = resolvers;
+  
