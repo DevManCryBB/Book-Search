72ce36d5bd9527fd417304af2c34ffbbf7e9226d
DevManCryBB <veronicagriggs3@gmail.com>
2023-06-20 16:49:33 -0700

    bug fixes

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import Navbar from './components/Navbar';
 
 // Create an Apollo Client instance
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql', 
+  uri: '/graphql', 
   cache: new InMemoryCache(),
 });
 
